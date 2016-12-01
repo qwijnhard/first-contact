b4d2a3a9ef27d3b758569bb2e1506b8bca0eb4bb
Quincy Wijnhard <quinhard@gmail.com>
2016-12-01 13:17:37 +0100

    restored & improved player

---
+++ game/Infested lab/Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+    public Rigidbody rBody;
+
+    private float inputH;
+    private float inputV;
+
+    private float moveX;
+    private float moveZ;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rBody.velocity = new Vector3(moveX, 0f, moveZ);
+    }
+
+    void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+
+        moveX = inputH * 500 * Time.deltaTime;
+        moveZ = inputV * 500 * Time.deltaTime;
+    }
+}
