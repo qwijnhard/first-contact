46cbaecda5e71fb9cd53e6473ef88dd1218a1eab
Quincy Wijnhard <quinhard@gmail.com>
2017-10-02 12:33:08 +0200

    Added incomplete state switching functionality to BasicMonster.cs Moved state switching logic to its own method Refined target selection: removed redundancies

--- game/Lab Outbreak/Assets/Scripts/Actors/NPC/Monster/BasicMonsterAI.cs
+++ game/Lab Outbreak/Assets/Scripts/Actors/NPC/Monster/BasicMonsterAI.cs
@@ -12,22 +12,26 @@ public class BasicMonsterAI : MonoBehaviour {
     private float attackRange;
 
     //object's AI thought process state
-    private string state = "Idle";
-
-    private string previousState; //use this if monsters go back to previous state if de-aggroed
-
-    //the eyes object
+    public string state = "Idle";
+    
+    //the eyes child-object
     public Transform eyes;
 
     //read positions of these objects to set as patrolling markers
     public GameObject[] patrolpoints;
     
     //object to be chased
-    private GameObject target;
+    public GameObject target;
     
     //determinants for random waypoint picking (time monster stands still)
     public float idleMin;
     public float idleMax;
+
+    //how long after losing sight the target remains targeted
+    public float chaseTimer = 3;
+    private float baseChaseTimer;
+    public float suspicionTimer = 5;
+    private float baseSuspicionTimer;
 
     //animation state triggers for the object's child object with an animator (see MonsterAnimScript.cs)
     public bool moving;
@@ -37,9 +41,21 @@ public class BasicMonsterAI : MonoBehaviour {
     //makes the coroutine trigger only once from being idle. without this, it would look for random points untill obj starts moving, causin stuttering.
     private bool coroutineTriggered = false;
     
+
+    /*
+     logic for damage source:
+     entity fires bullet. bullet has Tags.cs with string var "source" that gets the shooter's gameObject.name assigned somehow.
+     when hit, bullet sets monster's target to the damage source (possible in-game exploit: monster will always switch target to most recent damage source)
+         
+         */
+
+
 	void Start () {
 
         nav = GetComponent<NavMeshAgent>();
+
+        baseChaseTimer = chaseTimer;
+        baseSuspicionTimer = suspicionTimer;
 
 	}
 	
@@ -50,22 +66,23 @@ public class BasicMonsterAI : MonoBehaviour {
         {
             nav.SetDestination(target.transform.position);
         }
+
+        if (chaseTimer > 0) {
+            chaseTimer -= Time.deltaTime;
+        }
+        if(suspicionTimer > 0)
+        {
+            suspicionTimer -= Time.deltaTime;
+        }
+        
+
     }
 
     private void FixedUpdate()
     {
         if (state == "Idle")
         {
-            //set "target" to null
-            //[if you're not moving:]
-            //wait for a random amount of time between "idleMin" & "idleMax"
-            //select random waypoint, move to waypoint
-            //[/if you're not moving]
-            if(target != null)
-            {
-                target = null;
-            }
-
+            //if you're not moving: wait for a random amount of time between "idleMin" & "idleMax", then select random waypoint, move to waypoint
             if (!moving && !coroutineTriggered)
             {
                 coroutineTriggered = true;
@@ -76,19 +93,20 @@ public class BasicMonsterAI : MonoBehaviour {
             {
                 moving = false;
             }
+
             //if player enters sight, set state to "hostile"
-
-            
-
-            //if player directly damages, set state to "hostile"
+            //if player directly damages, set state to "Searching"
+            StateSwitch();// <=== method incomplete
         }
 
         if (state == "Searching")
         {
             //start "suspicion" timer, set waypoint near player, go to waypoint, wait, set new waypoint with decreased accuracy, repeat.
+
             //if "suspicion timer runs out, set state to Idle"
             //if player enters sight, set state to "hostile"
             //if player directly damages, set state to "hostile"
+            StateSwitch();
         }
 
         if (state == "Hostile")
@@ -97,22 +115,28 @@ public class BasicMonsterAI : MonoBehaviour {
             //if distance <= attackRange set chasing to false and attacking to true
             //while attacking, nav.setDestination(null); transform.LookAt(target.transform);
             //if player leaves sight radius, start "track" timer
-            //if track timer runs out, set state to "searching"
+
+            //if track timer runs out, set state to "Searching"
+            StateSwitch();
         }
 
         if (state == "Patrolling")
         {
             //move to each "patrolpoints" array and repeat.
+
             //if player enters sight, set state to "hostile"
-            //if player directly damages, set state to "hostile"
+            //if player directly damages, set state to "Searching"
+            StateSwitch();
         }
 
         if (state == "Animating")
         {
             //go to specific location with specific rotation
             //play an animation and repeat
+
             //if player enters sight, set state to "hostile"
-            //if player directly damages, set state to "hostile"
+            //if player directly damages, set state to "Searching"
+            StateSwitch();
         }
     }
 
@@ -122,7 +146,7 @@ public class BasicMonsterAI : MonoBehaviour {
         yield return new WaitForSeconds(rand);
         Vector3 randPos = Random.insideUnitSphere * 5;
         NavMeshHit navHit;
-        //input: me+randpos, output to navhit, use area size of 5f, on area of navmesh.allareas
+        //input: me+randpos, output to navhit, use area size of 10f, on area: navmesh.allareas
         NavMesh.SamplePosition(transform.position + randPos, out navHit, 10f, NavMesh.AllAreas);
         nav.SetDestination(navHit.position);
         moving = true;
@@ -135,23 +159,81 @@ public class BasicMonsterAI : MonoBehaviour {
 
         if (GetComponent<Tags>().alive)
         {
+            GameObject suspect;
+
             //if you see anything and you have no target, check its faction, then do either nothing or set it as "target"
-            if (target == null && eyesConeInput.GetComponent<Tags>() != null)
+            if (eyesConeInput.GetComponent<Tags>() != null)
             {
+                suspect = eyesConeInput;
                 if (eyesConeInput.GetComponent<Tags>().player)
                 {
                     //if the object's faction is player, set it as target, then "see" the target
-                    target = eyesConeInput;
                     RaycastHit rayHit;
-                    if (Physics.Linecast(eyes.position, target.transform.position, out rayHit))
+                    if (Physics.Linecast(eyes.position, suspect.transform.position, out rayHit))
                     {
-                        Debug.Log("player found");
+                        target = eyesConeInput;
+                        Debug.Log(target.name + " found");
+                        suspect = null;
+                        chaseTimer = baseChaseTimer;
+                        
                     }
                 }
             }
         }
 
         
+    }
+
+    private void StateSwitch()
+    {
+        //transitions out of "Idle"
+        //################################
+
+        //chaseTimer > 0, so i've seen a target. starting chase.
+        if(state == "Idle" && target != null && chaseTimer > 0)
+        {
+            state = "Hostile";
+        }
+
+        //chaseTimer <= 0, so i havent seen anything before now but something is there/hurt me.
+        if (state == "Idle" && target != null && chaseTimer <= 0)
+        {
+            //if [damage source] has Tags.player, target = [damage source]
+            suspicionTimer = baseSuspicionTimer;
+            state = "Searching";
+        }
+
+        //transitions out of "Searching"
+        //################################
+
+        //huh. must've run off.
+        if (state == "Searching" && suspicionTimer <= 0)
+        {
+            state = "Idle";
+
+            if (target != null)
+            {
+                target = null;
+            }
+        }
+
+        //chaseTimer > 0, so i've seen a target. starting chase.
+        if (state == "Searching" && chaseTimer > 0)
+        {
+            state = "Hostile";
+            
+        }
+
+        //transitions out of "Hostile"
+        //################################
+
+        if (state == "Hostile" && chaseTimer <= 0)
+        {
+            suspicionTimer = baseSuspicionTimer;
+            state = "Searching";
+
+        }
+
     }
 
 }
